0586c2bca61d94cbda5f621fb34cec1e924ccca4
TheodorFurtuna <80576577+TheodorFurtuna@users.noreply.github.com>
2021-11-22 17:10:01 +0200

    Add files via upload

--- clasa.cpp
+++ clasa.cpp
@@ -17,11 +17,11 @@ public:
     void citire_DFS()
     {
         int x, y;
-        cin >> noduri >> muchii;
+        in >> noduri >> muchii;
         lista.resize(noduri + 1);
         for (int i = 1; i <= muchii; i++)
         {
-            cin >> x >> y;
+            in >> x >> y;
             lista[x].push_back(y);
             lista[y].push_back(x);
         }
@@ -45,7 +45,7 @@ public:
                 nr++;
                 DFS(i, vizitat);
             }
-        cout << nr;
+        out << nr;
     }
     //BFS
     void BFS()
@@ -184,7 +184,7 @@ public:
         for (int i = afisare.size() - 1; i >= 0; i--)
             cout << afisare[i] << " ";
     }
-    //Conexiune critica
+    //Conexiune Critica
     void DFS_3(int timp, vector<vector<int>>& v, vector<int>& numarare, int k, int parinte, vector<bool>& vizite, vector<int>& vecin)
     {
         vizite[k] = 1;
@@ -221,6 +221,240 @@ public:
         DFS_3(timp, v, numarare, 0, -1, vizite, vecin);
         return lista;
     }
+    //Arbore Partial de Cost Minim
+    void APM()
+    {
+        vector<int> x, y, z, v, capat1, capat2;
+        in >> noduri >> muchii;
+        x.resize(muchii + 1);
+        y.resize(muchii + 1);
+        z.resize(muchii + 1);
+        v.resize(muchii + 1);
+        capat1.resize(muchii + 1);
+        capat2.resize(muchii + 1);
+        for (int i = 0; i <= muchii; i++)
+        {
+            x.push_back(0);
+            y.push_back(0);
+            z.push_back(0);
+            v.push_back(0);
+            capat1.push_back(0);
+            capat2.push_back(0);
+        }
+        for (int i = 1; i <= muchii; i++)
+            in >> x[i] >> y[i] >> z[i];
+        for (int i = 1; i <= noduri; i++)
+            v[i] = i;
+        for (int i = 1; i < muchii; i++)
+            for (int j = i + 1; j <= muchii; j++)
+            {
+                if (z[i] > z[j])
+                {
+                    swap(x[i], x[j]);
+                    swap(y[i], y[j]);
+                    swap(z[i], z[j]);
+                }
+            }
+        int k = 0, nr = 0, cost = 0, minn, maxx;
+        while (nr < noduri - 1)
+        {
+            k++;
+            if (v[x[k]] != v[y[k]])
+            {
+                nr++;
+                cost += z[k];
+                capat1[nr] = y[k];
+                capat2[nr] = x[k];
+                if (v[x[k]] > v[y[k]])
+                {
+                    minn = v[y[k]];
+                    maxx = v[x[k]];
+                }
+                else
+                {
+                    minn = v[x[k]];
+                    maxx = v[y[k]];
+                }
+                for (int i = 1; i <= noduri; i++)
+                    if (v[i] == maxx)
+                        v[i] = minn;
+                for (int i = 1; i <= noduri; i++)
+                    if (v[i] == v[minn])
+                        v[i] = v[maxx];
+            }
+        }
+        out << cost << "\n" << noduri - 1 << "\n";
+        for (int i = 1; i < noduri; i++)
+            out << capat1[i] << " " << capat2[i] << "\n";
+    }
+    //Dijkstra
+    void Djk()
+    {
+        int  x, y, cost, nr_noduri, p, q;
+        vector <int> costuri[5000], drumuri, h, d, lungime;
+        in >> noduri >> muchii;
+        drumuri.resize(muchii + 1);
+        h.resize(muchii + 1);
+        d.resize(muchii + 1);
+        lungime.resize(muchii + 1);
+        for (int i = 0; i <= muchii; i++)
+        {
+            drumuri.push_back(0);
+            h.push_back(0);
+            d.push_back(0);
+            lungime.push_back(0);
+            adiacenta[i].resize(noduri + 1);
+            costuri[i].resize(noduri + 1);
+        }
+        for (int i = 1; i <= muchii; i++)
+        {
+            in >> x >> y >> cost;
+            adiacenta[x].push_back(y);
+            costuri[x].push_back(cost);
+            if (i <= noduri)
+            {
+                drumuri[i] = 100000;
+                h[i] = i;
+                d[i] = i;
+            }
+            lungime[x] = adiacenta[x].size();
+        }
+        drumuri[1] = 0;
+        nr_noduri = noduri;
+        for (int i = 1; i <= noduri; i++)
+        {
+            for (int j = 0; j < lungime[h[1]]; j++)
+                if (drumuri[h[1]] + costuri[h[1]][j] < drumuri[adiacenta[h[1]][j]])
+                {
+                    drumuri[adiacenta[h[1]][j]] = drumuri[h[1]] + costuri[h[1]][j];
+                    p = d[adiacenta[h[1]][j]];
+                    q = 1;
+                    while (q == 1 && p > 1)
+                    {
+                        q = 0;
+                        if (drumuri[h[p]] < drumuri[h[p + 1]])
+                        {
+                            q = 1;
+                            swap(h[p], h[p + 1]);
+                            swap(d[h[p]], d[h[p + 1]]);
+                            p++;
+                        }
+                    }
+                }
+            d[h[1]] = nr_noduri;
+            h[1] = h[nr_noduri--];
+            p = 1;
+            while (1)
+            {
+                y = 0;
+                if (nr_noduri >= p * 2 + 1 && drumuri[h[p * 2 + 1]] < drumuri[h[p * 2]])
+                    y = 1;
+                if (nr_noduri >= p * 2 + y && drumuri[h[p]] > drumuri[h[p * 2 + y]])
+                {
+                    q = 1;
+                    swap(h[p], h[p * 2 + y]);
+                    swap(d[h[p]], d[h[p * 2 + y]]);
+                    p = p * 2 + y;
+                    continue;
+                }
+                break;
+            }
+        }
+        for (int i = 2; i <= noduri; i++)
+        {
+            if (drumuri[i] == 100000)
+                out << "0 ";
+            else
+                out << drumuri[i] << " ";
+        }
+    }
+    //Bellman-Ford
+    void BF()
+    {
+        vector <int> costuri[1000], distanta, v;
+        in >> noduri >> muchii;
+        distanta.resize(noduri + 1);
+        v.resize(noduri + 1);
+        for (int i = 0; i <= muchii; i++)
+        {
+            v.push_back(0);
+            adiacenta[i].resize(noduri + 1);
+            costuri[i].resize(noduri + 1);
+        }
+        int x, y, z;
+        for (int i = 1; i <= muchii; i++)
+        {
+            in >> x >> y >> z;
+            adiacenta[x].push_back(y);
+            costuri[x].push_back(z);
+        }
+        int final = 0, inceput = 0;
+        for (int i = 1; i <= noduri; i++)
+            distanta[i] = 10000;
+        v[inceput] = 1;
+        v[final] = 1;
+        distanta[1] = 0;
+        while (inceput <= final)
+        {
+            x = v[inceput];
+            inceput++;
+            for (int i = 0; i < adiacenta[x].size(); i++)
+                if (distanta[adiacenta[x][i]] > distanta[x] + costuri[x][i])
+                {
+                    distanta[adiacenta[x][i]] = distanta[x] + costuri[x][i];
+                    v[++final] = adiacenta[x][i];
+                }
+        }
+        for (int i = 2; i <= noduri; i++)
+            if (distanta[i] == 10000)
+                out << 0;
+            /*{
+                out << "Ciclu negativ!";
+                break;
+            }*/
+            else 
+                out << distanta[i] << " ";
+
+    }
+    //Paduri de multimi disjuncte
+    int get_root(int x, vector<int>multime)
+    {
+        while (multime[x] != x)
+            x = multime[x];
+        return x;
+    }
+    void Dis()
+    {
+        int nr_multimi, nr_operatii;
+        vector<int> multime;
+        in >> nr_multimi >> nr_operatii;
+        multime.resize(nr_operatii + 1);
+        for (int i = 0; i <= nr_operatii; i++)
+            multime.push_back(0);
+        for (int i = 1; i <= nr_multimi; i++)
+            multime[i] = i;
+        for (int i = 1; i <= nr_operatii; i++)
+        {
+            int cod, x, y, a, b;
+            in >> cod >> x >> y;
+            a = get_root(x, multime);
+            b = get_root(y, multime);
+            if (cod == 1)
+            {
+                if (a < b)
+                    multime[b] = a;
+                else 
+                    multime[a] = b;
+            }
+            else
+            {
+                if (a == b)
+                    out << "DA\n";
+                else 
+                    out << "NU\n";
+            }
+        }
+    }
 };
 int main()
 {
@@ -231,19 +465,39 @@ int main()
     cout << "2-BFS\n";
     cout << "3-Havel Hakimi\n";
     cout << "4-Componente Tare Conexe\n";
-    cout << "5-Sortare topologica\n";
-    cout << "6-Conexiune critica\n";
-    cout << "7-Componente biconexe\n";
+    cout << "5-Sortare Topologica\n";
+    cout << "6-Conexiune Critica\n";
+    cout << "7-Componente Biconexe\n";
+    cout << "8-Arbore Partial de Cost Minim\n";
+    cout << "9-Dijkstra\n";
+    cout << "10-Bellman-Ford\n";
+    cout << "11-Paduri de multimi disjuncte\n";
     cout << "Introduceti numarul problemei de rezolat:";
     cin >> nr_problema;
     if (nr_problema == 1)
     {
         g.citire_DFS();
         g.afisare_DFS();
+            /*
+            6 3
+            1 2
+            1 4
+            3 5
+            */
     }
     else if (nr_problema == 2)
     {
         g.BFS();
+            /*
+            5 7 2
+            1 2
+            2 1
+            2 2
+            3 2
+            2 5
+            5 3
+            4 5
+            */
     }
     else if (nr_problema == 3)
     {
@@ -252,11 +506,37 @@ int main()
     else if (nr_problema == 4)
     {
         g.CTC();
+            /*
+            8 12
+            1 2
+            2 6
+            6 7
+            7 6
+            3 1
+            3 4
+            2 3
+            4 5
+            5 4
+            6 5
+            5 8
+            8 7
+            */
     }
     else if (nr_problema == 5)
     {
         g.citire_DFS();
         g.ST();
+            /*
+            9 8
+            1 2
+            1 3
+            3 4
+            3 5
+            5 9
+            4 6
+            4 7
+            4 8
+            */
     }
     else if (nr_problema == 6)
     {
@@ -268,6 +548,69 @@ int main()
     {
         cout << "Problema aceasta nu a fost adaugata inca";
     }
+    else if (nr_problema == 8)
+    {
+        /*
+        9 14
+        1 2 10
+        1 3 -11
+        2 4 11
+        2 5 11
+        5 6 13
+        3 4 10
+        4 6 12
+        4 7 5
+        3 7 4
+        3 8 5
+        8 7 5
+        8 9 4
+        9 7 3
+        6 7 11
+        */
+        g.APM();
+    }
+    else if (nr_problema == 9)
+    {
+        /*
+        5 6
+        1 2 1
+        1 4 2
+        4 3 4
+        2 3 2
+        4 5 3
+        3 5 6
+        */
+        g.Djk();
+    }
+    else if (nr_problema == 10)
+    {
+    /*
+    5 8
+    1 3 -3
+    1 5 7
+    3 2 -2
+    3 4 7
+    5 1 4
+    5 2 3
+    5 3 4
+    4 5 3
+    */
+    g.BF();
+    }
+    else if (nr_problema == 11)
+    {
+    /*
+    4 6
+    1 1 2
+    1 3 4
+    2 1 3
+    2 1 2
+    1 1 3
+    2 1 4
+    */
+    g.Dis();
+    }
+    cout << "\nProblema " << nr_problema << " a fost rezolvata, verificati in date.out ce s-a afisat\n";
     in.close();
     out.close();
     return 0;
